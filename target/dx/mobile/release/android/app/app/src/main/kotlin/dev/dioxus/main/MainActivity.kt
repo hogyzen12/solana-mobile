ac88547bb748f0f76d5d2998186148e5b1fd4a5b
Alexander Binaei <bigti@Alexanders-MacBook-Pro.local>
2025-06-20 21:46:50 -0700

    reset

--- target/dx/mobile/release/android/app/app/src/main/kotlin/dev/dioxus/main/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package dev.dioxus.main;
-
-// need to re-export buildconfig down from the parent
-import com.example.Mobile.BuildConfig;
-typealias BuildConfig = BuildConfig;
-
-
-class MainActivity : WryActivity()
